675a5b7fd398b3b3cbffce108c67105bae1244c8
jessica8melo <jessica8melo@gmail.com>
2023-01-09 11:24:00 -0300

    ✅  teste checa ataques diagonais passou

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -1,17 +1,33 @@
 """Implementação de verificação se tabuleiro
 contém a solução do problema das 8 rainhas
 """
-def checaVertical(tabuleiro):
-    indices = []
-    for linha in tabuleiro:
-        i = linha.index('1')
-        if i in indices:
-            return 0
-        indices.append(i)
+def checa_diagonal(indices_linha, indices_coluna):
+    """Função que checa se há uma ataque diagonalmente"""
+    indice = 0
+    while indice < len(indices_coluna)-1:
+        cont = indice + 1
+        while cont < len(indices_linha):
+            delta_x = indices_linha[indice] - indices_linha[cont]
+            delta_y = indices_coluna[indice] - indices_coluna[cont]
+            if delta_x in (delta_y, -delta_y):
+                return 0
+            cont+=1
+        indice+=1
     return 1
 
+def checa_vertical(tabuleiro, indices_linha):
+    """Função que checa se há uma ataque verticalmente"""
+    for linha in tabuleiro:
+        i = linha.index('1')
+        if i in indices_linha:
+            return 0
+        indices_linha.append(i)
+    return indices_linha
 
-def checaHorizontal(tabuleiro):
+
+def checa_horizontal(tabuleiro, indices_coluna):
+    """Função que checa se há uma ataque horizontamenlte"""
+    i = 8
     for linha in tabuleiro:
         rainhas_linha = 0
         for caracter in linha:
@@ -19,21 +35,28 @@ def checaHorizontal(tabuleiro):
                 rainhas_linha += 1
         if rainhas_linha > 1:
             return 0
-    return 1
+        indices_coluna.append(i)
+        i -= 1
+    return indices_coluna
 
 def valida_entrada(tabuleiro):
+    """Função que valida se o tabuleiro é 8x8 e
+    se há exatamente 8 rainhas no tabuleiro"""
     rainhas = 0
-    for linha in tabuleiro: 
+    for linha in tabuleiro:
         if len(linha) != 8: #Verifica se é um tabuleiro 8x8
             return -1
         for caracter in linha: #Conta as rainhas no tabuleiro
             if caracter == '1':
                 rainhas += 1
     if rainhas != 8: #Verifica se não há 8 rainhas no tabuleiro
-        return -1       
+        return -1
 
 def main():
+    """Função que recebe a entrada e chama as condições de validação"""
     tabuleiro = []
+    indices_linha = []
+    indices_coluna = []
     n_linha = 8
     while n_linha > 0:
         linha = input(f"Descreva a linha {n_linha} do seu tabuleiro: ")
@@ -41,10 +64,12 @@ def main():
         n_linha -= 1
     if valida_entrada(tabuleiro) == -1:
         return "-1 (Entrada inválida)"
-    if checaHorizontal(tabuleiro) == 0:
+    if checa_horizontal(tabuleiro, indices_coluna) == 0:
         return "0 (Não é solução)"
-    if checaVertical(tabuleiro) == 0:
+    if checa_vertical(tabuleiro, indices_linha) == 0:
         return "0 (Não é solução)"
-    return "Por enquanto, dando certo"
+    if checa_diagonal(indices_linha, indices_coluna) == 0:
+        return "0 (Não é solução)"
+    return "1 (É solução)"
 
 print(main())
--- test_oito_rainhas.py
+++ test_oito_rainhas.py
@@ -1,24 +1,33 @@
-from oito_rainhas import valida_entrada, checaHorizontal, checaVertical
+"""Testes da implementação"""
+from oito_rainhas import valida_entrada, checa_horizontal, checa_vertical, checa_diagonal
 
-def test_valida_entrada(): 
-    tabuleiro1 = ['00001000', '01000000', '00010000', '00000010',
+def test_valida_entrada():
+    """Faz a validação da função valida_entrada com testes de um tabuleiro
+    incompleto e um tabuleiro sem 8 rainhas"""
+    tabuleiro1 = ['10001000', '01000000', '00010000', '00000010',
                 '00100000', '00000001', '00000100']
     tabuleiro2 = ['00001000', '01000000', '00010000', '00000010',
                 '00100000', '00000001', '00000100', '00000000']
-    assert valida_entrada(tabuleiro1) == -1 #Tabuleiro incompleto
-    assert valida_entrada(tabuleiro2) == -1 #Tabuleiro sem 8 rainhas
+    assert valida_entrada(tabuleiro1) == -1
+    assert valida_entrada(tabuleiro2) == -1
 
-def test_checaHorizontal():
+def test_checa_horizontal():
+    """Faz a validação da função checa_horizontal com testes de um tabuleiro
+    com ataque horizontal"""
     tabuleiro = ['00001001', '01000000', '00010000', '00000010',
                 '00100000', '00000001', '00000100','00000000']
-    assert checaHorizontal(tabuleiro) == 0 #Ataque horizontal
+    assert checa_horizontal(tabuleiro) == 0
 
-def test_checaVertizal():
+def test_checa_vertical():
+    """Faz a validação da função checa_vertical com testes de um tabuleiro
+    com ataque vertical"""
     tabuleiro = ['01000000', '01000000', '00010000', '00000010',
                 '00100000', '00000001', '00000100','10000000']
-    assert checaVertical(tabuleiro) == 0 #Ataque vertical
+    assert checa_vertical(tabuleiro) == 0
 
-def test_checaDiagonal():
+def test_checa_diagonal():
+    """Faz a validação da função checa_diagonal com testes de um tabuleiro
+    com ataque diagonal"""
     tabuleiro = ['00001000', '00010000', '01000000', '00000010',
                 '00100000', '00000001', '00000100', '10000000']
-    assert checaDiagonal(tabuleiro) == 0 #Ataque diagonal
+    assert checa_diagonal(tabuleiro) == 0
