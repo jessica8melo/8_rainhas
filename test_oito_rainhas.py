f8e69b5553eceee125ce31a877a9c8c1393cecd1
jessica8melo <jessica8melo@gmail.com>
2023-01-08 23:36:27 -0300

    🧪 teste checa ataques horizontais falhando

--- test_oito_rainhas.py
+++ test_oito_rainhas.py
@@ -1,4 +1,4 @@
-from oito_rainhas import valida_entrada
+from oito_rainhas import valida_entrada, checaHorizontal
 
 def test_valida_entrada(): 
     tabuleiro1 = ['00001000', '01000000', '00010000', '00000010',
@@ -7,3 +7,8 @@ def test_valida_entrada():
                 '00100000', '00000001', '00000100', '00000000']
     assert valida_entrada(tabuleiro1) == -1 #Tabuleiro incompleto
     assert valida_entrada(tabuleiro2) == -1 #Tabuleiro sem 8 rainhas
+
+def test_checaHorizontal():
+    tabuleiro = ['00001001', '01000000', '00010000', '00000010',
+                '00100000', '00000001', '00000000']
+    assert checaHorizontal(tabuleiro) == -1
